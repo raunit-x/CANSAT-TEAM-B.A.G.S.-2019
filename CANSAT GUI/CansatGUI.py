63b7d2813854efa132315ddac9c13daf929b9d01
Raunit <36741947+raunit-1804@users.noreply.github.com>
2019-05-11 16:51:59 +0530

    Update CansatGUI.py

--- CANSAT GUI/CansatGUI.py
+++ CANSAT GUI/CansatGUI.py
@@ -11,7 +11,6 @@ import dash_table
 import serial
 import csv
 
-# Making use of the deque data structure so as to ensure that the computations are capped.
 MAXLEN = 20
 X = deque(maxlen=MAXLEN)
 X.append(0)
@@ -21,52 +20,38 @@ Voltage = deque(maxlen=MAXLEN)
 Altitude = deque(maxlen=MAXLEN)
 Pitch = deque(maxlen=MAXLEN)
 Roll = deque(maxlen=MAXLEN)
+GpsSats = deque(maxlen=MAXLEN)
+BladeSpinRate = deque(maxlen=MAXLEN)
+BonusDirection = deque(maxlen=MAXLEN)
 
-# Making a dictionary just for easier reference
-values = {'1': Temperature, '2': Pressure, '3': Voltage,
-          '4': Altitude, '5': Pitch, '6': Roll
+# This dictionary maps a number to the deque of a field along with the min and max of the graphing window
+# of the field. Index 1 is for min values and Index 2 for max values and index 3 is for the
+# index of the field in the .csv header file
+values = {'1': [Temperature, 0, 0, 5], '2': [Pressure, 0, 0, 4], '3': [Voltage, 0, 0, 6],
+          '4': [Altitude, 0, 0, 3], '5': [Pitch, 0, 0, 12], '6': [Roll, 0, 0, 13],
+          '7': [GpsSats, 0, 0, 11], '8': [BladeSpinRate, 0, 0, 14], '9': [BonusDirection, 0, 0, 16]
           }
-# These are used to calculate the width and height of the graph windows
-# The graph windows' size changes depending upon the current max and min of the deque of the particular field.
-min_temp = 0
-max_temp = 0
-max_pressure = 0
-min_pressure = 0
-min_volt = 0
-max_volt = 0
-min_altitude = 0
-max_altitude = 0
-min_pitch = 0
-max_pitch = 0
-min_roll = 0
-max_roll = 0
-
-# Initialising all the parameters of the pyserial connection
 SERIAL_PORT = '/dev/tty.usbserial-AL017DBD'
 BAUD_RATE = 9600
 time_out = 0.99
 
-# This is the header row for the Dataframe which would be printed in the GUI
-# This is also the header row for the .csv file being created
 HEADER_ROW = ['TEAM_ID', 'MISSION_TIME', 'PACKET COUNT', 'ALTITUDE', 'PRESSURE',
               'TEMPERATURE', 'VOLTAGE', 'GPS TIME', 'GPS LATITUDE', 'GPS LONGITUDE', 'GPS ALTITUDE',
               'GPS SATELLITES', 'PITCH', 'ROLL', 'BLADE SPIN RATE', 'SOFTWARE STATE', 'BONUS DIRECTION']
+n = len(HEADER_ROW)
 
-# File path to the .csv file
 file_name = '/Users/raunitsingh/Desktop/cansat.csv'
 
-# initialising the dataframe with the header row
 df = pd.DataFrame(columns=HEADER_ROW)
+# df.append(HEADER_ROW)
 
-# Establishing the connection by searching for the port
 # if __name__ == '__main__':
 #     ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=time_out)
 
 myFile = open(file_name, 'wb')  # open in binary
 writer = csv.writer(myFile)
-# writer.writerow(HEADER_ROW)
+writer.writerow(HEADER_ROW)
 
-# Initialise the dash 'app'
 app = dash.Dash(__name__)
 # colors dictionary for all the colors which would be needed in styling
 colors = {
@@ -81,10 +66,8 @@ colors = {
 # encoding all the images in base64
 encoded_image_bags = base64.b64encode(open('/Users/raunitsingh/Desktop/Cansat Stuff/CANSAT GUI/logo.jpeg', 'rb').read())
 encoded_image_flag = base64.b64encode(open('/Users/raunitsingh/Desktop/Cansat Stuff/CANSAT GUI/flag.png', 'rb').read())
-encoded_image_cansat = base64.b64encode(
-    open('/Users/raunitsingh/Desktop/Cansat Stuff/CANSAT GUI/CanSat Logo - Color.png', 'rb').read())
-encoded_image_nsut = base64.b64encode(
-    open('/Users/raunitsingh/Desktop/Cansat Stuff/CANSAT GUI/nsut_logo.png', 'rb').read())
+encoded_image_cansat = base64.b64encode(open('/Users/raunitsingh/Desktop/Cansat Stuff/CANSAT GUI/CanSat Logo - Color.png', 'rb').read())
+encoded_image_nsut = base64.b64encode(open('/Users/raunitsingh/Desktop/Cansat Stuff/CANSAT GUI/nsut_logo.png', 'rb').read())
 
 # definition of the app
 app.layout = html.Div(style={'backgroundColor': colors['background'], 'text-align': 'center'}, children=[
@@ -100,10 +83,10 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], 'text-alig
     # The header 2
     html.H2(
         id='gcs',
-        children='Ground Control Station',
+        children='GROUND CONTROL STATION',
         style={
             'textAlign': 'center',
-            'color': '#f0b629'
+            'color': '#9b9999'  # dark grey
         }
     ),
     # All the images encapsulated under 'images'
@@ -138,6 +121,18 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], 'text-alig
         ),
         dcc.Graph(
             id='roll-live',
+            animate=True
+        ),
+        dcc.Graph(
+            id='gps-live',
+            animate=True
+        ),
+        dcc.Graph(
+            id='blade-live',
+            animate=True
+        ),
+        dcc.Graph(
+            id='bonus-live',
             animate=True
         ),
         dcc.Interval(
@@ -178,6 +173,10 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], 'text-alig
 
 ])
 
+# COMMENT OUT THIS
+fileDF = pd.read_csv('/Users/raunitsingh/Desktop/Cansat Stuff/Flight_1516_practice.csv')
+fileArray = fileDF.values
+count = 0
 
 # The callback for updating all the graphs
 @app.callback([Output('temperature-live', 'figure'),
@@ -186,94 +185,99 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], 'text-alig
                Output('altitude-live', 'figure'),
                Output('pitch-live', 'figure'),
                Output('roll-live', 'figure'),
+               Output('gps-live', 'figure'),
+               Output('blade-live', 'figure'),
+               Output('bonus-live', 'figure'),
                Output('datatable-container', 'data')],
               [Input('interval-component', 'n_intervals')])
 def update_graph(n):
-    global values, min_temp, max_temp, min_altitude, min_pitch, max_altitude, max_pitch, min_pressure, \
-        max_pressure, min_volt, max_volt, min_roll, max_roll, df
+    global values, df, X, count, fileArray
     X.append(X[-1] + 1)
     # Here the sensor updating code will be there
-    #     message = ser.readline()
-    #     message = message[:-2]  # to remove '\r' and '\n'
-    data = ['1516', '0', '17', '177.66', '99209.27', '19.91', '0', '2013-10-22T01:37:56+05:30', '0', '0', '0', '0',
-            '0', '0', '0', 'Boot', '0']
-    indices_values = ['1', '2', '3', '4', '5', '6']
-    indices_data = [5, 4, 6, 3, 12, 13]
+    # message = ser.readline()
+    # message = message[:-2]  # to remove '\r' and '\n'
+    # Converting the message to a list
+    # data = list(message.split(','))
+    data = fileArray[count % 32]
+    count += 1
+    indices_values = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    indices_data = []
+    for index in range(len(indices_values)):
+        indices_data.append(values[indices_values[index]][3])
+    # Updating the deque by adding the corresponding values to the corresponding arrays
     if len(data) > 16:
-        # data = data[:-2]
         print(data)
-        for index, j in zip(indices_data, indices_values):
-            values[j].append(float(data[index]))
+        for i, j in zip(indices_values, indices_data):
+            values[i][0].append(data[j])
         writer.writerow(data)
+    else:
+        writer.writerow(df[df.shape[0] - 1])  # write the previous row again
 
     # Use of so many variables for optimisation over using min(deque) and max(deque)
     if len(values['1']) == 1:
-        min_temp = Temperature[0]
-        max_temp = Temperature[0]
-        min_pressure = Pressure[0]
-        max_pressure = Pressure[0]
-        min_volt = Voltage[0]
-        max_volt = Voltage[0]
-        min_altitude = Altitude[0]
-        max_altitude = Altitude[0]
-        min_pitch = Pitch[0]
-        max_pitch = Pitch[0]
-        min_roll = Roll[0]
-        max_roll = Roll[0]
-    # Since it is a stream of bits, this gets rid of the huge overhead by using min(Array)
-    #  and max(Array) for all the deques
-    # The number of comparisons are reduced to 2 per deque instead of 40.
-    min_temp = min(float(data[5]), min_temp)
-    max_temp = max(float(data[5]), max_temp)
-    min_pressure = min(float(data[4]), min_pressure)
-    max_pressure = max(float(data[4]), max_pressure)
-    min_volt = min(float(data[6]), min_volt)
-    max_volt = max(float(data[6]), max_volt)
-    min_altitude = min(float(data[3]), min_altitude)
-    max_altitude = max(float(data[3]), max_altitude)
-    min_pitch = min(float(data[12]), min_pitch)
-    max_pitch = max(float(data[12]), max_pitch)
-    min_roll = min(float(data[13]), min_roll)
-    max_roll = max(float(data[13]), max_roll)
+        for i, j in zip(indices_values, indices_data):
+            values[i][1] = values[i][2] = data[j]
+    # Calculating the min and max values for the graph windows
+    for i, j in zip(indices_values, indices_data):
+        values[i][1] = min(values[i][1], data[j])
+        values[i][2] = max(values[i][2], data[j])
 
     data_temperature = go.Scatter(
         x=list(X),
-        y=list(values['1']),
+        y=list(values['1'][0]),
         name='Scatter',
         mode='lines+markers'
     )
     data_pressure = go.Scatter(
         x=list(X),
-        y=list(values['2']),
+        y=list(values['2'][0]),
         name='Scatter',
         mode='lines+markers'
     )
     data_voltage = go.Scatter(
         x=list(X),
-        y=list(values['3']),
+        y=list(values['3'][0]),
         name='Scatter',
         mode='lines+markers'
     )
     data_altitude = go.Scatter(
         x=list(X),
-        y=list(values['4']),
+        y=list(values['4'][0]),
         name='Scatter',
         mode='lines+markers'
     )
     data_pitch = go.Scatter(
         x=list(X),
-        y=list(values['5']),
+        y=list(values['5'][0]),
         name='Scatter',
         mode='lines+markers'
     )
     data_roll = go.Scatter(
         x=list(X),
-        y=list(values['6']),
+        y=list(values['6'][0]),
         name='Scatter',
         mode='lines+markers'
     )
-    min_x = min(X)
-    max_x = max(X)
+    data_gps_sats = go.Scatter(
+        x=list(X),
+        y=list(values['7'][0]),
+        name='Scatter',
+        mode='lines+markers'
+    )
+    data_blade_spin_rate = go.Scatter(
+        x=list(X),
+        y=list(values['8'][0]),
+        name='Scatter',
+        mode='lines+markers'
+    )
+    data_bonus_direction = go.Scatter(
+        x=list(X),
+        y=list(values['9'][0]),
+        name='Scatter',
+        mode='lines+markers'
+    )
+    min_x = X[0]
+    max_x = X[-1]
     layout_temperature = {
         'title': 'TEMPERATURE(in Celcius)',
         'plot_bgcolor': colors['GraphSpace'],
@@ -281,8 +285,7 @@ def update_graph(n):
         'font': {'color': '#669999'},
         'colorway': ['#FFFF00'],
         'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=[min_x, max_x]),
-        'yaxis': dict(linecolor='#669999', linewidth=2, title='Temperature',
-                      range=[min_temp - 0.03, max_temp + 0.03])
+        'yaxis': dict(linecolor='#669999', linewidth=2, title='Temperature')
     }
     layout_pressure = {
         'title': 'PRESSURE(in Pascals)',
@@ -291,8 +294,7 @@ def update_graph(n):
         'font': {'color': '#669999'},
         'colorway': ['#FFFF00'],
         'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=[min_x, max_x]),
-        'yaxis': dict(linecolor='#669999', linewidth=2, title='Pressure',
-                      range=[min_pressure - 0.03, max_pressure + 0.03])
+        'yaxis': dict(linecolor='#669999', linewidth=2, title='Pressure')
     }
     layout_voltage = {
         'title': 'Voltage(in Volts)',
@@ -301,8 +303,7 @@ def update_graph(n):
         'font': {'color': '#669999'},
         'colorway': ['#FFFF00'],
         'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=[min_x, max_x]),
-        'yaxis': dict(linecolor='#669999', linewidth=2, title='Voltage',
-                      range=[min_volt - 0.03, max_volt + 0.03])
+        'yaxis': dict(linecolor='#669999', linewidth=2, title='Voltage')
     }
     layout_altitude = {
         'title': 'ALTITUDE(in meters)',
@@ -311,8 +312,7 @@ def update_graph(n):
         'font': {'color': '#669999'},
         'colorway': ['#FFFF00'],
         'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=[min_x, max_x]),
-        'yaxis': dict(linecolor='#669999', linewidth=2, title='Altitude',
-                      range=[min_altitude - 0.03, max_altitude + 0.03])
+        'yaxis': dict(linecolor='#669999', linewidth=2, title='Altitude')
     }
     layout_pitch = {
         'title': 'PITCH(in Radians)',
@@ -321,8 +321,7 @@ def update_graph(n):
         'font': {'color': '#669999'},
         'colorway': ['#FFFF00'],
         'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=[min_x, max_x]),
-        'yaxis': dict(linecolor='#669999', linewidth=2, title='Pitch',
-                      range=[min_pitch - 0.03, max_pitch + 0.03])
+        'yaxis': dict(linecolor='#669999', linewidth=2, title='Pitch')
     }
     layout_roll = {
         'title': 'ROLL(in Radians)',
@@ -332,25 +331,54 @@ def update_graph(n):
         'colorway': ['#FFFF00'],
         'height': '60%',
         'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=[min_x, max_x]),
-        'yaxis': dict(linecolor='#669999', linewidth=2, title='Roll',
-                      range=[min_roll - 0.03, max_roll + 0.03])
+        'yaxis': dict(linecolor='#669999', linewidth=2, title='Roll')
     }
-    # storing all the layouts and the data of the graph in variables
+    layout_gps_sats = {
+        'title': 'GPS Satellites(integer value)',
+        'plot_bgcolor': colors['GraphSpace'],
+        'paper_bgcolor': colors['background'],
+        'font': {'color': '#669999'},
+        'colorway': ['#FFFF00'],
+        'height': '60%',
+        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=[min_x, max_x]),
+        'yaxis': dict(linecolor='#669999', linewidth=2, title='GPS Satellites')
+    }
+    layout_blade_spin_rate = {
+        'title': 'BLADE SPIN RATE(in radians/sec)',
+        'plot_bgcolor': colors['GraphSpace'],
+        'paper_bgcolor': colors['background'],
+        'font': {'color': '#669999'},
+        'colorway': ['#FFFF00'],
+        'height': '60%',
+        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=[min_x, max_x]),
+        'yaxis': dict(linecolor='#669999', linewidth=2, title='Blade Spin Rate')
+    }
+    layout_bonus_direction = {
+        'title': 'BONUS DIRECTION(in Radians)',
+        'plot_bgcolor': colors['GraphSpace'],
+        'paper_bgcolor': colors['background'],
+        'font': {'color': '#669999'},
+        'colorway': ['#FFFF00'],
+        'height': '60%',
+        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=[min_x, max_x]),
+        'yaxis': dict(linecolor='#669999', linewidth=2, title='Bonus Direction')
+    }
+
     temp = {'data': [data_temperature], 'layout': layout_temperature}
     pres = {'data': [data_pressure], 'layout': layout_pressure}
     volt = {'data': [data_voltage], 'layout': layout_voltage}
     alti = {'data': [data_altitude], 'layout': layout_altitude}
     pitc = {'data': [data_pitch], 'layout': layout_pitch}
     roll = {'data': [data_roll], 'layout': layout_roll}
-
+    gps = {'data': [data_gps_sats], 'layout': layout_gps_sats}
+    blade = {'data': [data_blade_spin_rate], 'layout': layout_blade_spin_rate}
+    bonus = {'data': [data_bonus_direction], 'layout': layout_bonus_direction}
     # Implementing the DataFrame as a deque
     if df.shape[0] > 20:  # if the number of rows is > 20, then we pop the first row
         df = df.iloc[1:]  # Limiting the size of the DataFrame to 20 rows
     df = df.append(pd.Series(data, index=df.columns), ignore_index=True)
+    return temp, pres, volt, alti, pitc, roll, gps, blade, bonus, df.to_dict('rows')
 
-    return temp, pres, volt, alti, pitc, roll, df.to_dict('rows')
 
-
-# Runnign the app on port = 8050 with debugger = False
 if __name__ == '__main__':
     app.run_server(port=8050)
