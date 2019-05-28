cadfdf68d90676368290794b44b3041b1f66fb43
Raunit <36741947+raunit-x@users.noreply.github.com>
2019-05-28 16:16:33 +0530

    Update CansatGUI.py

--- CANSAT GUI/CansatGUI.py
+++ CANSAT GUI/CansatGUI.py
@@ -1,4 +1,3 @@
-# Importing all the libraries and modules
 import dash
 import base64
 from dash.dependencies import Output, Input
@@ -11,44 +10,85 @@ import dash_table
 import serial
 import csv
 
-MAXLEN = 20
-X = deque(maxlen=MAXLEN)
+
+X = []
 X.append(0)
-Temperature = deque(maxlen=MAXLEN)
-Pressure = deque(maxlen=MAXLEN)
-Voltage = deque(maxlen=MAXLEN)
-Altitude = deque(maxlen=MAXLEN)
-Pitch = deque(maxlen=MAXLEN)
-Roll = deque(maxlen=MAXLEN)
-GpsSats = deque(maxlen=MAXLEN)
-BladeSpinRate = deque(maxlen=MAXLEN)
-BonusDirection = deque(maxlen=MAXLEN)
+Temperature = []
+Pressure = []
+Voltage = []
+Altitude = []
+Pitch = []
+Roll = []
+GpsSats = []
+BladeSpinRate = []
+BonusDirection = []
 
 # This dictionary maps a number to the deque of a field along with the min and max of the graphing window
 # of the field. Index 1 is for min values and Index 2 for max values and index 3 is for the
 # index of the field in the .csv header file
-values = {'1': [Temperature, 0, 0, 5], '2': [Pressure, 0, 0, 4], '3': [Voltage, 0, 0, 6],
-          '4': [Altitude, 0, 0, 3], '5': [Pitch, 0, 0, 12], '6': [Roll, 0, 0, 13],
-          '7': [GpsSats, 0, 0, 11], '8': [BladeSpinRate, 0, 0, 14], '9': [BonusDirection, 0, 0, 16]
+values = {'1': [Temperature, 0, 0, 5], '2': [Pressure, 0, 0, 4],
+          '3': [Voltage, 0, 0, 6], '4': [Altitude, 0, 0, 3], '5': [Pitch, 0, 0, 12],
+          '6': [Roll, 0, 0, 13], '7': [GpsSats, 0, 0, 11],
+          '8': [BladeSpinRate, 0, 0, 14], '9': [BonusDirection, 0, 0, 16]
           }
-SERIAL_PORT = '/dev/tty.usbserial-AL017DBD'
-BAUD_RATE = 9600
-time_out = 0.99
+# SERIAL_PORT = '/dev/cu.usbserial-AL017DBE'
+# #  '/dev/tty.usbserial-AL017DBD'
+# BAUD_RATE = 9600
+# time_out = 0.99
+
+my_settings = {'PORT': '/dev/cu.usbserial-AL017DBE', 'BAUD_RATE': 9600, 'TIME_OUT': 0.99}
 
 HEADER_ROW = ['TEAM_ID', 'MISSION_TIME', 'PACKET COUNT', 'ALTITUDE', 'PRESSURE',
               'TEMPERATURE', 'VOLTAGE', 'GPS TIME', 'GPS LATITUDE', 'GPS LONGITUDE', 'GPS ALTITUDE',
               'GPS SATELLITES', 'PITCH', 'ROLL', 'BLADE SPIN RATE', 'SOFTWARE STATE', 'BONUS DIRECTION']
-n = len(HEADER_ROW)
+
 
 file_name = '/Users/raunitsingh/Desktop/cansat.csv'
 
 df = pd.DataFrame(columns=HEADER_ROW)
 # df.append(HEADER_ROW)
 
-# if __name__ == '__main__':
-#     ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=time_out)
 
-myFile = open(file_name, 'wb')  # open in binary
+class Controller:
+    def __init__(self, settings):
+        self.ser = None
+        self.settings = settings
+
+    def connect_disconnect(self):
+        try:
+            if self.ser is None:
+                self.ser = serial.Serial(port=self.settings['PORT'],
+                                         baudrate=self.settings['BAUD_RATE'], timeout=self.settings['TIME_OUT'])
+                print "Successfully connected to port %r." % self.ser.port
+                return True
+            else:
+                if self.ser.isOpen():
+                    self.ser.close()
+                    print "Disconnected."
+                    return False
+                else:
+                    self.ser = serial.Serial(port=self.settings['PORT'],
+                                             baudrate=self.settings['BAUD_RATE'], timeout=self.settings['TIME_OUT'])
+                    print "Connected."
+                    return True
+        except serial.SerialException, e:
+            return False
+
+    def is_connected(self):
+        try:
+            return self.ser.isOpen()
+        except:
+            return False
+
+
+
+
+my_connector = Controller(my_settings)
+
+if __name__ == '__main__':
+    my_connector.connect_disconnect()
+
+myFile = open(file_name, 'a')  # open in write in bytes
 writer = csv.writer(myFile)
 writer.writerow(HEADER_ROW)
 
@@ -66,8 +106,10 @@ colors = {
 # encoding all the images in base64
 encoded_image_bags = base64.b64encode(open('/Users/raunitsingh/Desktop/Cansat Stuff/CANSAT GUI/logo.jpeg', 'rb').read())
 encoded_image_flag = base64.b64encode(open('/Users/raunitsingh/Desktop/Cansat Stuff/CANSAT GUI/flag.png', 'rb').read())
-encoded_image_cansat = base64.b64encode(open('/Users/raunitsingh/Desktop/Cansat Stuff/CANSAT GUI/CanSat Logo - Color.png', 'rb').read())
-encoded_image_nsut = base64.b64encode(open('/Users/raunitsingh/Desktop/Cansat Stuff/CANSAT GUI/nsut_logo.png', 'rb').read())
+encoded_image_cansat = base64.b64encode(open('/Users/raunitsingh/Desktop/Cansat Stuff/CANSAT GUI/'
+                                             'CanSat Logo - Color.png', 'rb').read())
+encoded_image_nsut = base64.b64encode(open('/Users/raunitsingh/Desktop/Cansat Stuff/CANSAT GUI/'
+                                           'nsut_logo.png', 'rb').read())
 
 # definition of the app
 app.layout = html.Div(style={'backgroundColor': colors['background'], 'text-align': 'center'}, children=[
@@ -96,6 +138,17 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], 'text-alig
         html.Img(id='cansat', src='data:image/png;base64,{}'.format(encoded_image_cansat)),
         html.Img(id='nsut', src='data:image/png;base64,{}'.format(encoded_image_nsut))
     ], className='Images'),
+
+    # Add a reset button
+    html.Div([html.Button('RESET', id='button')]),
+    html.Br(),
+    html.Br(),
+    # Add an input box to write to serial
+    html.Div([
+        html.Button(id='connect-disconnect', type='submit', value=None, children='DISCONNECT'),
+    ]),
+
+    html.Div([html.P(id='terminal_output')]),
 
     # All the graphs which would be updated using a callback
     html.Div([
@@ -173,10 +226,20 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], 'text-alig
 
 ])
 
-# COMMENT OUT THIS
-fileDF = pd.read_csv('/Users/raunitsingh/Desktop/Cansat Stuff/Flight_1516_practice.csv')
-fileArray = fileDF.values
-count = 0
+
+indices_values = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+indices_data = []
+for index in range(len(indices_values)):
+    indices_data.append(values[indices_values[index]][3])
+
+
+resetClickCount = 0
+connectClickCount = 0
+firstTime = True
+# paragraphData = list([])
+string_paragraph = str('')
+button_name = 'DISCONNECT'
+
 
 # The callback for updating all the graphs
 @app.callback([Output('temperature-live', 'figure'),
@@ -188,39 +251,60 @@ count = 0
                Output('gps-live', 'figure'),
                Output('blade-live', 'figure'),
                Output('bonus-live', 'figure'),
-               Output('datatable-container', 'data')],
-              [Input('interval-component', 'n_intervals')])
-def update_graph(n):
-    global values, df, X, count, fileArray
-    X.append(X[-1] + 1)
+               Output('datatable-container', 'data'),
+               Output('connect-disconnect', 'children')],
+              [Input('interval-component', 'n_intervals'),
+               Input('button', 'n_clicks'),
+               Input('connect-disconnect', 'n_clicks')]
+              )
+def update_graph(n, n_clicks, n_clicks_connector):
+    print('N : {}'.format(n))
+    global values, df, X, indices_data, indices_values, resetClickCount, firstTime, connectClickCount, my_connector,\
+        button_name
     # Here the sensor updating code will be there
-    # message = ser.readline()
-    # message = message[:-2]  # to remove '\r' and '\n'
-    # Converting the message to a list
-    # data = list(message.split(','))
-    data = fileArray[count % 32]
-    count += 1
-    indices_values = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-    indices_data = []
-    for index in range(len(indices_values)):
-        indices_data.append(values[indices_values[index]][3])
-    # Updating the deque by adding the corresponding values to the corresponding arrays
-    if len(data) > 16:
-        print(data)
-        for i, j in zip(indices_values, indices_data):
-            values[i][0].append(data[j])
-        writer.writerow(data)
-    else:
-        writer.writerow(df[df.shape[0] - 1])  # write the previous row again
+    message = ''
+    port_not_open = False
+    try:
+        if my_connector.ser.isOpen():
+            message = my_connector.ser.readline()
+    except:
+        print('Exception: Port not found')
+        my_connector.ser = None
+        message = 'Port not open!'
+        port_not_open = True
 
-    # Use of so many variables for optimisation over using min(deque) and max(deque)
-    if len(values['1']) == 1:
+    while port_not_open:
+        try:
+            print('Tried')
+            if my_connector.connect_disconnect():
+                port_not_open = False
+        except:
+            pass
+
+    print(message)
+    message = message[:-2]  # to remove '\r' and '\n'
+
+    # print(n_clicks)
+
+    if firstTime:
+        if n_clicks is not None:
+            resetClickCount = n_clicks
+        if n_clicks_connector is not None:
+            connectClickCount = n_clicks_connector
+        firstTime = False
+
+    data = list(message.split(','))  # Converting the message to a list
+
+    # The length of the data should be 17 and the first value should be 1516 which is the team_id
+    if len(data) == 17 and data[0] == '1516':
+        print('DATA : {}'.format(data))
         for i, j in zip(indices_values, indices_data):
-            values[i][1] = values[i][2] = data[j]
-    # Calculating the min and max values for the graph windows
-    for i, j in zip(indices_values, indices_data):
-        values[i][1] = min(values[i][1], data[j])
-        values[i][2] = max(values[i][2], data[j])
+            values[i][0].append(float(data[j]))
+        writer.writerow(data)
+        X.append(X[-1] + 1)
+        if df.shape[0] > 20:  # if the number of rows is > 20, then we pop the first row
+            df = df.iloc[1:]  # Limiting the size of the DataFrame to 20 rows
+        df = df.append(pd.Series(data, index=df.columns), ignore_index=True)
 
     data_temperature = go.Scatter(
         x=list(X),
@@ -276,15 +360,16 @@ def update_graph(n):
         name='Scatter',
         mode='lines+markers'
     )
-    min_x = X[0]
-    max_x = X[-1]
+    min_x = max(0, X[-1] - 200)
+    max_x = min(X[-1], min_x + 200)
+    range_x = [min_x, max_x]
     layout_temperature = {
         'title': 'TEMPERATURE(in Celcius)',
         'plot_bgcolor': colors['GraphSpace'],
         'paper_bgcolor': colors['background'],
         'font': {'color': '#669999'},
         'colorway': ['#FFFF00'],
-        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=[min_x, max_x]),
+        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=range_x),
         'yaxis': dict(linecolor='#669999', linewidth=2, title='Temperature')
     }
     layout_pressure = {
@@ -293,7 +378,7 @@ def update_graph(n):
         'paper_bgcolor': colors['background'],
         'font': {'color': '#669999'},
         'colorway': ['#FFFF00'],
-        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=[min_x, max_x]),
+        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=range_x),
         'yaxis': dict(linecolor='#669999', linewidth=2, title='Pressure')
     }
     layout_voltage = {
@@ -302,7 +387,7 @@ def update_graph(n):
         'paper_bgcolor': colors['background'],
         'font': {'color': '#669999'},
         'colorway': ['#FFFF00'],
-        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=[min_x, max_x]),
+        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=range_x),
         'yaxis': dict(linecolor='#669999', linewidth=2, title='Voltage')
     }
     layout_altitude = {
@@ -311,7 +396,7 @@ def update_graph(n):
         'paper_bgcolor': colors['background'],
         'font': {'color': '#669999'},
         'colorway': ['#FFFF00'],
-        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=[min_x, max_x]),
+        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=range_x),
         'yaxis': dict(linecolor='#669999', linewidth=2, title='Altitude')
     }
     layout_pitch = {
@@ -320,7 +405,7 @@ def update_graph(n):
         'paper_bgcolor': colors['background'],
         'font': {'color': '#669999'},
         'colorway': ['#FFFF00'],
-        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=[min_x, max_x]),
+        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=range_x),
         'yaxis': dict(linecolor='#669999', linewidth=2, title='Pitch')
     }
     layout_roll = {
@@ -330,7 +415,7 @@ def update_graph(n):
         'font': {'color': '#669999'},
         'colorway': ['#FFFF00'],
         'height': '60%',
-        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=[min_x, max_x]),
+        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=range_x),
         'yaxis': dict(linecolor='#669999', linewidth=2, title='Roll')
     }
     layout_gps_sats = {
@@ -340,7 +425,7 @@ def update_graph(n):
         'font': {'color': '#669999'},
         'colorway': ['#FFFF00'],
         'height': '60%',
-        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=[min_x, max_x]),
+        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=range_x),
         'yaxis': dict(linecolor='#669999', linewidth=2, title='GPS Satellites')
     }
     layout_blade_spin_rate = {
@@ -350,7 +435,7 @@ def update_graph(n):
         'font': {'color': '#669999'},
         'colorway': ['#FFFF00'],
         'height': '60%',
-        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=[min_x, max_x]),
+        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=range_x),
         'yaxis': dict(linecolor='#669999', linewidth=2, title='Blade Spin Rate')
     }
     layout_bonus_direction = {
@@ -360,7 +445,7 @@ def update_graph(n):
         'font': {'color': '#669999'},
         'colorway': ['#FFFF00'],
         'height': '60%',
-        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=[min_x, max_x]),
+        'xaxis': dict(linecolor='#669999', linewidth=2, title='Mission Time', range=range_x),
         'yaxis': dict(linecolor='#669999', linewidth=2, title='Bonus Direction')
     }
 
@@ -373,12 +458,28 @@ def update_graph(n):
     gps = {'data': [data_gps_sats], 'layout': layout_gps_sats}
     blade = {'data': [data_blade_spin_rate], 'layout': layout_blade_spin_rate}
     bonus = {'data': [data_bonus_direction], 'layout': layout_bonus_direction}
-    # Implementing the DataFrame as a deque
-    if df.shape[0] > 20:  # if the number of rows is > 20, then we pop the first row
-        df = df.iloc[1:]  # Limiting the size of the DataFrame to 20 rows
-    df = df.append(pd.Series(data, index=df.columns), ignore_index=True)
-    return temp, pres, volt, alti, pitc, roll, gps, blade, bonus, df.to_dict('rows')
+
+    app.logger.info(resetClickCount)
+    # Reset button logic
+    if n_clicks is not None and n_clicks > resetClickCount:
+        for j in indices_values:
+            values[j][0] = []  # Clear all the deques
+        X = list([])
+        X.append(0)
+        resetClickCount = n_clicks  # update the count of the reset clicks
+        print('RESET!')
+
+    if n_clicks_connector is not None and n_clicks_connector > connectClickCount:
+        print('Button Clicked!')
+        is_connected = my_connector.connect_disconnect()
+        if is_connected:
+            button_name = 'DISCONNECT'
+        else:
+            button_name = 'RECONNECT'
+        connectClickCount = n_clicks_connector
+
+    return temp, pres, volt, alti, pitc, roll, gps, blade, bonus, df.to_dict('rows'), button_name
 
 
 if __name__ == '__main__':
-    app.run_server(port=8050)
+    app.run_server(port=8051)
